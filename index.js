cb71eac340a5434ab1cc867bf4289335159c62f3
RobertBReich <mail@robertreich.de>
2022-07-15 12:56:48 +0200

    build season Overview

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
-console.log('hello world!');
+console.clear();
+
+let arrSeasonData = [];
+
+function resetSeasonOverview(){
+    //
+}
+
+function filterSeasonData(){
+    //
+    const apiURL = "https://rickandmortyapi.com/api/episode";
+    fetch(apiURL)
+    .then(response => response.json())
+    .then((data) => {
+
+        arrSeasonData = data.results;
+        buildSeasonView();
+    });
+
+}
+
+function buildSeasonView(){
+    //
+    buildEpisodeView();
+}
+
+function resetEpisodeView(){
+    //
+}
+
+function buildEpisodeView(){
+    
+    const dynamicSeasonContainer = document.querySelector('[data-js="episodeList"]');
+
+    arrSeasonData.forEach((episode) => {
+
+        const dynamicSeasonItem = document.createElement("li");
+        const dynamicSeasonButton = document.createElement("button");  
+        dynamicSeasonItem.classList.add("episode-list__li");
+        dynamicSeasonButton.classList.add("episode-list__button");
+        dynamicSeasonItem.append(dynamicSeasonButton);
+        dynamicSeasonContainer.append(dynamicSeasonItem);
+        dynamicSeasonButton.innerText = episode.name;
+
+
+    });
+}
+
+
+
+function showSeasonOverview (seasonNumber=1) {
+
+    console.log(seasonNumber);
+    resetSeasonOverview()
+ 
+    /* s01e01 */
+    filterSeasonData()
+    
+}
+
+
+function showEpisodeView (episodeNumber=1) {
+
+    console.log(episodeNumber);
+    resetEpisodeView();
+    buildEpisodeView();
+}
+
+showSeasonOverview();
+
+showEpisodeView();
+
